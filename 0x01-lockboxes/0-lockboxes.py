18778a848754db35fa9f89e3794aea125c0adb8d
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-01-12 02:21:34 +0300

    Unlock Boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-
 """
 Determines if all the boxes can be opened
 Returns True or False
 """
-
 def canUnlockAll(boxes):
-    unlocked = False
+    len = len(boxes)
+    visited = set()
+    queue = [0]
 
-    return unlocked
+    while queue:
+        current_box = queue.pop(0)
+        visited.add(current_box)
+        for key in boxes[current_box]:
+            if key < len and key not in visited:
+                queue.append(key)
+
+    return len(visited) == len
