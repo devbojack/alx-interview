429f3a165eb819b4b86717e44f2a3afe80fa3136
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-01-17 23:12:18 +0300

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,4 +2,8 @@
 """Minimum Operations: Copy All and Paste"""
 
 def minOperations(n):
+    """calculates the fewest number of operations needed 
+    to result in exactly n H characters
+    Returns an int or 0
+    """
     return 0
