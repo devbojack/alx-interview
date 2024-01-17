9ec277df0f1d66dacbadcc394b801bb9d4443aac
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-01-17 23:37:31 +0300

    Min operations - fix pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Minimum Operations: Copy All and Paste"""
 
+
 def minOperations(n):
-    """calculates the fewest number of operations needed 
+    """calculates the fewest number of operations needed
     to result in exactly n H characters
     Returns an int or 0
     """
