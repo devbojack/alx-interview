2dc99223e46fd30b70d57d82a92470806a3f86e1
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-01-25 19:16:53 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,36 +2,40 @@
 """Reads stdin line by line and computes metrics"""
 import sys
 
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-lines_processed = 0
+if __name__ == '__main__':
 
-try:
-    for line in sys.stdin:
-        # Split the line into components
-        parts = line.split()
+    total_size, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-        # Check if the line follows the specified format
-        if len(parts) >= 9 and parts[8].isdigit():
-            # Extract relevant information
-            status_code = int(parts[8])
-            file_size = int(parts[9])
+    def print_stats(stats: dict, total_size: int) -> None:
+        print("File size: {:d}".format(total_size))
+        for code, count in sorted(stats.items()):
+            if count:
+                print("{}: {}".format(code, count))
 
-            # Update metrics
-            total_size += file_size
-            status_codes[status_code] += 1
-            lines_processed += 1
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
 
-        # Print statistics every 10 lines
-        if lines_processed % 10 == 0:
-            print(f"File size: {total_size}")
-            for code in sorted(status_codes.keys()):
-                if status_codes[code] > 0:
-                    print(f"{code}: {status_codes[code]}")
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except IndexError:
+                pass
 
-except KeyboardInterrupt:
-    # Handle keyboard interruption and print final statistics
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
+            try:
+                total_size += int(data[-1])
+            except (ValueError, IndexError):
+                pass
+
+            if count % 10 == 0:
+                print_stats(stats, total_size)
+
+        print_stats(stats, total_size)
+
+    except KeyboardInterrupt:
+        print_stats(stats, total_size)
+        raise
