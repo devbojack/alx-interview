c355f6c6b9a0efd135cdf73de84ecd2c7e241954
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-02-08 23:54:39 +0300

    n queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""N queens"""
+
+import sys
+
+
+def is_safe(board, row, col, N):
+    """Is Safe?"""
+    for i in range(row):
+        if board[i] == col:
+            return False
+
+        if abs(board[i] - col) == abs(i - row):
+            return False
+
+    return True
+
+
+def solve_n_queens_util(board, row, N, solutions):
+    """Solve Queens Util"""
+    if row == N:
+        solutions.append([[i, board[i]] for i in range(N)])
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row] = col
+            solve_n_queens_util(board, row + 1, N, solutions)
+
+
+def solve_n_queens(N):
+    """Solve n queens"""
+    if not N.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    N = int(N)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * N
+    solutions = []
+    solve_n_queens_util(board, 0, N, solutions)
+
+    for solution in solutions:
+        print(solution)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    N = sys.argv[1]
+    solve_n_queens(N)
