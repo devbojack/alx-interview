b49149b4c7c3f6844bc211d6d2005804922b2379
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-02-15 21:17:15 +0300

    Star wars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,38 +2,24 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
+const filmID = process.argv[2];
 
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+async function starwarsCharacters(filmId) {
+    try {
+        const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+        const response = await request(endpoint);
+        const filmData = JSON.parse(response.body);
+        const characters = filmData.characters;
 
-request(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-        return;
-    }
-
-    if (response.statusCode !== 200) {
-        console.error('Invalid response:', response.statusCode);
-        return;
-    }
-
-    const filmData = JSON.parse(body);
-    const charactersUrls = filmData.characters;
-
-    charactersUrls.forEach(characterUrl => {
-        request(characterUrl, (error, response, body) => {
-            if (error) {
-                console.error('Error:', error);
-                return;
-            }
-
-            if (response.statusCode !== 200) {
-                console.error('Invalid response:', response.statusCode);
-                return;
-            }
-
-            const characterData = JSON.parse(body);
+        for (let i = 0; i < characters.length; i++) {
+            const urlCharacter = characters[i];
+            const characterResponse = await request(urlCharacter);
+            const characterData = JSON.parse(characterResponse.body);
             console.log(characterData.name);
-        });
-    });
-});
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+starwarsCharacters(filmID);
