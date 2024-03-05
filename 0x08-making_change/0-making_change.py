b845813cb7198777262b78e6df95a7c587d24cdc
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-03-05 19:47:31 +0300

    Change from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,14 +5,13 @@ Change comes from within
 
 
 def makeChange(coins, total):
+    """
+    Make changes
+    """
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
+    dp = [0] + [float("inf")] * (total)
     for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[-1] if dp[-1] != float("inf") else -1
